de88086ab2c4f618201a421b9c3aeab90781483b
shabiqghazi <sgarkaan02@gmail.com>
2022-07-26 22:20:58 +0700

    66 Deploy

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,43 @@
 <template>
   <div>
-    <!-- <demo-one />
-    <demo-two /> -->
-    <!-- <composition-methods /> -->
-    <!-- <composition-v-model /> -->
-    <!-- <composition-computed /> -->
-    <!-- <composition-watchers /> -->
-    <!-- <parent-component /> -->
-    <!-- <composition-template-refs /> -->
-    <!-- <person-component /> -->
+    <demo-one />
+    <demo-two />
+    <composition-methods />
+    <composition-v-model />
+    <composition-computed />
+    <composition-watchers />
+    <parent-component />
+    <composition-template-refs />
+    <person-component />
     <click-counter />
     <hover-counter />
   </div>
 </template>
 
 <script>
-// import DemoOne from "./components/DemoOne.vue";
-// import DemoTwo from "./components/DemoTwo.vue";
-// import CompositionMethods from "./components/Methods.vue";
-// import CompositionVModel from "./components/CompositionVModel.vue";
-// import CompositionComputed from "./components/CompositionComputed.vue";
-// import CompositionWatchers from "./components/CompositionWatchers.vue";
-// import ParentComponent from "./components/Parent.vue";
-// import CompositionTemplateRefs from "./components/CompositionTemplateRefs.vue";
-// import PersonComponent from "./components/Person.vue";
+import DemoOne from "./components/DemoOne.vue";
+import DemoTwo from "./components/DemoTwo.vue";
+import CompositionMethods from "./components/Methods.vue";
+import CompositionVModel from "./components/CompositionVModel.vue";
+import CompositionComputed from "./components/CompositionComputed.vue";
+import CompositionWatchers from "./components/CompositionWatchers.vue";
+import ParentComponent from "./components/Parent.vue";
+import CompositionTemplateRefs from "./components/CompositionTemplateRefs.vue";
+import PersonComponent from "./components/Person.vue";
 import ClickCounter from "./components/ClickCounter.vue";
 import HoverCounter from "./components/HoverCounter.vue";
 export default {
   name: "App",
   components: {
-    // DemoOne,
-    // DemoTwo,
-    // CompositionMethods,
-    // CompositionVModel,
-    // CompositionComputed,
-    // CompositionWatchers,
-    // ParentComponent,
-    // CompositionTemplateRefs,
-    // PersonComponent,
+    DemoOne,
+    DemoTwo,
+    CompositionMethods,
+    CompositionVModel,
+    CompositionComputed,
+    CompositionWatchers,
+    ParentComponent,
+    CompositionTemplateRefs,
+    PersonComponent,
     ClickCounter,
     HoverCounter,
   },
